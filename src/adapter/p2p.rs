51241da9c3fc3f76373affee4065f0c06e45fed6
kotagit75 <unity75lets5@gmail.com>
2026-03-20 20:43:54 +0900

    add p2p

---
+++ src/adapter/p2p.rs
@@ -0,0 +1,41 @@
+use axum::{
+    Router,
+    extract::{self, State},
+    routing::post,
+};
+use serde::{Deserialize, Serialize};
+use tokio::sync::mpsc::Sender;
+
+use crate::{
+    model::{event::Event, proof::ProofPool, stamp::Stamp},
+    util::key::PK,
+};
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub enum P2PMessage {
+    None,
+    RequestStamp(PK, usize /*difficulty */),
+    ResponceStamp(PK, Stamp),
+    UpdateProofpool(ProofPool),
+}
+
+const P2P_PORT: u32 = 3000;
+pub async fn init_p2p(tx: Sender<Event>) {
+    // add_peer("localhost".to_string()).await;
+    let app = Router::new()
+        .route("/", post(handle_message))
+        .with_state(tx);
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", P2P_PORT))
+        .await
+        .unwrap();
+    info!("P2P server is running on http://localhost:{}", P2P_PORT);
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handle_message(
+    State(tx): State<Sender<Event>>,
+    extract::Json(message): extract::Json<P2PMessage>,
+) -> &'static str {
+    let _ = tx.send(Event::P2PRequest(message));
+    ""
+}
