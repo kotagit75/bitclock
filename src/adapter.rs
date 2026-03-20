c447e231f44c2a36dbf1f519b8de639c6039184d
kotagit75 <unity75lets5@gmail.com>
2026-03-20 20:44:08 +0900

    add init_adapter

--- src/adapter.rs
+++ src/adapter.rs
@@ -1,1 +1,11 @@
+use tokio::sync::mpsc::Sender;
+
+use crate::{adapter::p2p::init_p2p, model::event::Event};
+
 pub mod p2p;
+
+pub fn init_adapter(tx: Sender<Event>) {
+    tokio::spawn(async move {
+        init_p2p(tx).await;
+    });
+}
