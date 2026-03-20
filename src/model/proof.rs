2d11259884365a251312511b0dc1c929153f6d2d
kotagit75 <unity75lets5@gmail.com>
2026-03-20 19:10:31 +0900

    add proof model

---
+++ src/model/proof.rs
@@ -0,0 +1,25 @@
+use std::collections::HashSet;
+
+use serde::{Deserialize, Serialize};
+
+use crate::{
+    model::{address::Address, signature::Signature, stamp::Stamp},
+    util::key::SK,
+};
+
+#[derive(Clone, Debug, Serialize, Deserialize, Hash)]
+pub struct Proof {
+    pub data: String,
+    pub stamps: Vec<Stamp>,
+    pub sk: SK,
+    pub address: Address,
+    pub difficulty: usize,
+    pub time: i64,
+    pub sign: Signature,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct ProofPool {
+    pub pool: HashSet<Proof>,
+}
+pub type UnStampedProof = Proof;
