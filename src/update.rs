535533d4301a9b445cc48238902bd26856d3b8bc
kotagit75 <unity75lets5@gmail.com>
2026-03-20 20:43:29 +0900

    update update function

--- src/update.rs
+++ src/update.rs
@@ -1,7 +1,12 @@
+use crate::adapter::p2p::P2PMessage;
 use crate::model::{effect::Effect, event::Event, state::State};
 
 pub fn update(state: State, event: Event) -> (State, Effect) {
     match event {
         Event::None => (state, Effect::None),
+        Event::P2PRequest(P2PMessage::None) => (state, Effect::None),
+        Event::P2PRequest(P2PMessage::RequestStamp(pk, difficulty)) => (state, Effect::None),
+        Event::P2PRequest(P2PMessage::ResponceStamp(pk, stamp)) => (state, Effect::None),
+        Event::P2PRequest(P2PMessage::UpdateProofpool(new_pool)) => (state, Effect::None),
     }
 }
