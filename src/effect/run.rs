03b8463cb91b196e342f7511644ede9f768c89b1
kotagit75 <unity75lets5@gmail.com>
2026-03-20 20:57:51 +0900

    Added a branch to the processing in run_effect

--- src/effect/run.rs
+++ src/effect/run.rs
@@ -1,3 +1,10 @@
 use crate::model::effect::Effect;
 
-pub async fn run_effect(effect: Effect) {}
+pub async fn run_effect(effect: Effect) {
+    match effect {
+        Effect::None => {}
+        Effect::CreateStamp(pk, difficulty) => {
+            // create_stamp and broadcast
+        }
+    }
+}
