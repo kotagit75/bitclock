26fef54ef0160b775943410d1833c53c449b216d
kotagit75 <unity75lets5@gmail.com>
2026-03-21 10:56:53 +0900

    add add_to_un_signed_proof_pool

--- src/core/state.rs
+++ src/core/state.rs
@@ -1,5 +1,9 @@
 use crate::{
-    model::{proof::ProofPool, stamp::Stamp, state::State},
+    model::{
+        proof::{ProofPool, UnSignedProof},
+        stamp::Stamp,
+        state::State,
+    },
     util::key::PK,
 };
 
@@ -8,7 +12,7 @@ impl State {
         State {
             proof_pool: pool,
             stamp_pool: self.stamp_pool.clone(),
-            un_signed_proof: self.un_signed_proof.clone(),
+            un_signed_proof_pool: self.un_signed_proof_pool.clone(),
             count: count,
             node_sk: self.node_sk.clone(),
             address: self.address.clone(),
@@ -16,7 +20,7 @@ impl State {
         }
     }
     pub fn add_to_stamp_pool(&self, pk: PK, stamp: Stamp) -> Self {
-        if self.un_signed_proof.get(&pk).is_none() {
+        if self.un_signed_proof_pool.get(&pk).is_none() {
             return self.clone();
         }
         let mut new_stamp_pool = self.stamp_pool.clone();
@@ -24,7 +28,23 @@ impl State {
         State {
             proof_pool: self.proof_pool.clone(),
             stamp_pool: new_stamp_pool,
-            un_signed_proof: self.un_signed_proof.clone(),
+            un_signed_proof_pool: self.un_signed_proof_pool.clone(),
+            count: self.count,
+            node_sk: self.node_sk.clone(),
+            address: self.address.clone(),
+            peers: self.peers.clone(),
+        }
+    }
+    pub fn add_tp_un_signed_proof_pool(&self, un_signed_proof: UnSignedProof) -> Self {
+        let mut new_un_signed_proof_pool = self.un_signed_proof_pool.clone();
+        let Ok(pk) = un_signed_proof.get_proof_pk() else {
+            return self.clone();
+        };
+        new_un_signed_proof_pool.insert(pk, un_signed_proof);
+        State {
+            proof_pool: self.proof_pool.clone(),
+            stamp_pool: self.stamp_pool.clone(),
+            un_signed_proof_pool: new_un_signed_proof_pool,
             count: self.count,
             node_sk: self.node_sk.clone(),
             address: self.address.clone(),
