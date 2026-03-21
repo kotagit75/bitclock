2ae02ab044622f92f831f203beb489f1ee6a2566
kotagit75 <unity75lets5@gmail.com>
2026-03-21 09:32:48 +0900

    add effect: Broadcast

--- src/model/effect.rs
+++ src/model/effect.rs
@@ -1,6 +1,7 @@
-use crate::util::key::PK;
+use crate::{adapter::p2p::P2PMessage, util::key::PK};
 
 pub enum Effect {
     None,
     CreateStamp(PK, usize /*difficulty */),
+    Broadcast(P2PMessage),
 }
