3cab085a27cd42250c9a3d6e10ff99c5848b8562
kotagit75 <unity75lets5@gmail.com>
2026-03-21 10:00:09 +0900

    update State init

--- src/core/state.rs
+++ src/core/state.rs
@@ -4,6 +4,8 @@ impl State {
     pub fn update_pool_and_count(&self, (pool, count): (ProofPool, u32)) -> Self {
         State {
             proof_pool: pool,
+            stamp_pool: self.stamp_pool.clone(),
+            un_signed_proof: self.un_signed_proof.clone(),
             count: count,
             node_sk: self.node_sk.clone(),
             address: self.address.clone(),
