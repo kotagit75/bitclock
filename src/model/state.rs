0ab347daa36f15f94cd929562900be89775ceabc
kotagit75 <unity75lets5@gmail.com>
2026-03-21 09:59:31 +0900

    change stamp_pool and un_signed_proof type: Vec -> HashMap

--- src/model/state.rs
+++ src/model/state.rs
@@ -1,11 +1,20 @@
+use std::collections::HashMap;
+
 use crate::{
-    model::{address::Address, client::Client, proof::ProofPool},
-    util::key::SK,
+    model::{
+        address::Address,
+        client::Client,
+        proof::{ProofPool, UnSignedProof},
+        stamp::Stamp,
+    },
+    util::key::{PK, SK},
 };
 
 #[derive(Clone)]
 pub struct State {
     pub proof_pool: ProofPool,
+    pub stamp_pool: HashMap<PK, Stamp>,
+    pub un_signed_proof: HashMap<PK, UnSignedProof>,
     pub count: u32,
     pub node_sk: SK,
     pub address: Address,
