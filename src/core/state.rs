0040e191222d1bbe1527986e691b11771064d3b1
kotagit75 <unity75lets5@gmail.com>
2026-03-21 09:50:50 +0900

    add update_pool_and_count

--- src/core/state.rs
+++ src/core/state.rs
@@ -1,10 +1,10 @@
 use crate::model::{proof::ProofPool, state::State};
 
 impl State {
-    pub fn update_pool(&self, pool: ProofPool) -> Self {
+    pub fn update_pool_and_count(&self, (pool, count): (ProofPool, u32)) -> Self {
         State {
             proof_pool: pool,
-            count: self.count,
+            count: count,
             node_sk: self.node_sk.clone(),
             address: self.address.clone(),
             peers: self.peers.clone(),
