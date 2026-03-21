1a243723208c234c07f7e89dee684ba80acf08a6
kotagit75 <unity75lets5@gmail.com>
2026-03-21 10:15:11 +0900

    add event: APIRequest(APIRequest)

--- src/model/event.rs
+++ src/model/event.rs
@@ -1,7 +1,8 @@
-use crate::adapter::p2p::P2PMessage;
+use crate::adapter::{api::APIRequest, p2p::P2PMessage};
 
 #[derive(Debug)]
 pub enum Event {
     None,
     P2PRequest(P2PMessage),
+    APIRequest(APIRequest),
 }
