ca00e5f0a0aff3ed0ad6fe09a7b6e5d52f4c13af
kotagit75 <unity75lets5@gmail.com>
2026-03-21 10:05:53 +0900

    add add_to_stamp_pool

--- src/core/state.rs
+++ src/core/state.rs
@@ -1,4 +1,7 @@
-use crate::model::{proof::ProofPool, state::State};
+use crate::{
+    model::{proof::ProofPool, stamp::Stamp, state::State},
+    util::key::PK,
+};
 
 impl State {
     pub fn update_pool_and_count(&self, (pool, count): (ProofPool, u32)) -> Self {
@@ -7,6 +10,19 @@ impl State {
             stamp_pool: self.stamp_pool.clone(),
             un_signed_proof: self.un_signed_proof.clone(),
             count: count,
+            node_sk: self.node_sk.clone(),
+            address: self.address.clone(),
+            peers: self.peers.clone(),
+        }
+    }
+    pub fn add_to_stamp_pool(&self, pk: PK, stamp: Stamp) -> Self {
+        let mut new_stamp_pool = self.stamp_pool.clone();
+        new_stamp_pool.insert(pk, stamp);
+        State {
+            proof_pool: self.proof_pool.clone(),
+            stamp_pool: new_stamp_pool,
+            un_signed_proof: self.un_signed_proof.clone(),
+            count: self.count,
             node_sk: self.node_sk.clone(),
             address: self.address.clone(),
             peers: self.peers.clone(),
