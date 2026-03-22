5ee27b77aba6a95ec0189824c16579ab8e620063
kotagit75 <unity75lets5@gmail.com>
2026-03-22 15:27:39 +0900

    add memory usage to SystemStatus

--- src/util/status.rs
+++ src/util/status.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use sysinfo::{Pid, System};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub enum SystemStatusType {
@@ -7,10 +8,15 @@ pub enum SystemStatusType {
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct SystemStatus {
     pub status: SystemStatusType,
+    pub memory_usage_bytes: Option<u64>,
 }
 
 pub fn get_status() -> SystemStatus {
+    let mut sys = System::new_all();
+    sys.refresh_all();
+    let pid = Pid::from_u32(std::process::id());
     SystemStatus {
         status: SystemStatusType::Running,
+        memory_usage_bytes: sys.process(pid).and_then(|process| Some(process.memory())),
     }
 }
