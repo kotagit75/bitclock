88c6862b432234bbaa674cf0ddc3b0afa0d69b99
kotagit75 <unity75lets5@gmail.com>
2026-03-24 14:56:49 +0900

    add bitclock.rs

---
+++ src/bitclock.rs
@@ -0,0 +1,28 @@
+use clap::Parser;
+
+use crate::{
+    boot::{Args, boot},
+    effect::loop_effect_run,
+    update::update,
+};
+
+pub async fn start() {
+    let args = Args::parse();
+    simple_logger::init_with_level(args.level).unwrap();
+    let Ok((mut state, (mut event_rx, state_tx))) = boot(args).await else {
+        return;
+    };
+    while let Some((new_state, effect)) = event_rx.recv().await.and_then(|event| {
+        debug!("Event received: {:?}", event);
+        Some(update(
+            state.clone(),
+            event,
+            chrono::prelude::Utc::now().timestamp_millis(),
+        ))
+    }) {
+        state = new_state.clone();
+        let _ = state_tx.send(state.clone());
+        let state_clone = state.clone();
+        tokio::spawn(async move { loop_effect_run(state_clone, effect).await });
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,10 @@
 extern crate log;
 extern crate simple_logger as logger;
 
-use clap::Parser;
-
-use crate::{
-    boot::{Args, boot},
-    effect::loop_effect_run,
-    update::update,
-};
+use crate::bitclock::start;
 
 mod adapter;
+mod bitclock;
 mod boot;
 mod core;
 mod effect;
@@ -20,22 +15,5 @@ mod util;
 
 #[tokio::main]
 async fn main() {
-    let args = Args::parse();
-    simple_logger::init_with_level(args.level).unwrap();
-    let Ok((mut state, (mut event_rx, state_tx))) = boot(args).await else {
-        return;
-    };
-    while let Some((new_state, effect)) = event_rx.recv().await.and_then(|event| {
-        debug!("Event received: {:?}", event);
-        Some(update(
-            state.clone(),
-            event,
-            chrono::prelude::Utc::now().timestamp_millis(),
-        ))
-    }) {
-        state = new_state.clone();
-        let _ = state_tx.send(state.clone());
-        let state_clone = state.clone();
-        tokio::spawn(async move { loop_effect_run(state_clone, effect).await });
-    }
+    start().await;
 }
