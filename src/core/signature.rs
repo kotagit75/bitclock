7dae914b27a2c6a091cd88c5185a7a06030469bd
kotagit75 <unity75lets5@gmail.com>
2026-03-29 06:41:07 +0900

    verify function returns Result

--- src/core/signature.rs
+++ src/core/signature.rs
@@ -21,16 +21,16 @@ pub fn sign(data: &[u8], sk: SK) -> Result<Signature, ()> {
     Ok(sign)
 }
 
-pub fn verify(data: &[u8], pk: PK, signature: Signature) -> bool {
+pub fn verify(data: &[u8], pk: PK, signature: Signature) -> Result<(), ()> {
     let key = pk.key();
     let Ok(mut verifyer) = Verifier::new(MessageDigest::sha256(), &key) else {
-        return false;
+        return Err(());
     };
     let Ok(_) = verifyer.update(&data) else {
-        return false;
+        return Err(());
     };
     let Ok(result) = verifyer.verify(&signature) else {
-        return false;
+        return Err(());
     };
-    result
+    Ok(())
 }
