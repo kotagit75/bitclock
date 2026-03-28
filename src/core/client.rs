aa6f458bcee28014280033021e2fb41a89e54804
kotagit75 <unity75lets5@gmail.com>
2026-03-29 07:54:20 +0900

    add test in client

--- src/core/client.rs
+++ src/core/client.rs
@@ -5,3 +5,14 @@ impl Client {
         Client { ip_addr }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_client_new() {
+        let client = Client::new("127.0.0.1".to_string());
+        assert_eq!(client.ip_addr, "127.0.0.1");
+    }
+}
