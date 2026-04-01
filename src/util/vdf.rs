8ef12620135466c31f7a855d52d31d7b141502ef
kotagit75 <unity75lets5@gmail.com>
2026-04-01 17:00:35 +0900

    add VDF_BITS

--- src/util/vdf.rs
+++ src/util/vdf.rs
@@ -1,16 +1,14 @@
 use vdf::{PietrzakVDFParams, VDF, VDFParams};
 
-//
 const VDF_DIFFICULTY: u64 = 68;
+const VDF_BITS: u16 = 512;
 
 pub fn verify_solution(challenge: &[u8], solution: &[u8]) -> bool {
-    let vdf = PietrzakVDFParams(1024).new();
-    let result = vdf.verify(challenge, VDF_DIFFICULTY, solution).is_ok();
-    println!("{:?}", result);
-    result
+    let vdf = PietrzakVDFParams(VDF_BITS).new();
+    vdf.verify(challenge, VDF_DIFFICULTY, solution).is_ok()
 }
 
 pub fn solve(challenge: &[u8]) -> Result<Vec<u8>, vdf::InvalidIterations> {
-    let vdf = PietrzakVDFParams(1024).new();
+    let vdf = PietrzakVDFParams(VDF_BITS).new();
     vdf.solve(challenge, VDF_DIFFICULTY)
 }
